95af3cb158503c9eba7981f370f22c920d69e7d3
vickellar <vickellar.01@gmail.com>
2026-03-11 01:29:15 +0200

    modifications

--- src/main/java/com/bypass/bypasstransers/config/MailConfig.java
+++ src/main/java/com/bypass/bypasstransers/config/MailConfig.java
@@ -1,9 +1,7 @@
 package com.bypass.bypasstransers.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+
 
 @Configuration
 public class MailConfig {
--- src/main/java/com/bypass/bypasstransers/controller/UserController.java
+++ src/main/java/com/bypass/bypasstransers/controller/UserController.java
@@ -20,7 +20,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
